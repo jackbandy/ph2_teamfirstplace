31ae3f9ce627bf9c484aaef7a00da51660a45a3b
derekschlabach <derekschlabach@gmail.com>
2015-03-20 22:40:22 -0500

    added RETest.py

---
+++ RETest.py
@@ -0,0 +1,13 @@
+import re
+
+print "Input a Regular Expression"
+re = re.compile(raw_input(">>"))
+
+while True:
+    print "Enter a test input (quit to exit the program)"
+    input = raw_input(">>")
+    if (input == ("quit")):
+        break
+    print (input == re.match(input).group()) and (input != "")
+
+    
