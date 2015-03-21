747e65a432596674a4e68236095383a73d6c7b7b
Jack Bandy <jackbandy@icloud.com>
2015-03-21 16:52:04 -0500

    For merge

--- PromptPlot.py
+++ PromptPlot.py
@@ -16,14 +16,36 @@ loaded = Solution.load(form.bf(), "sample")
 
 
 class Plot:
+    def prompt(self):
+        return "Yo options to plot now iz: u1, u2, p, stream, mesh, error"
+
+    def getDict(self):
+        return { "u1": Phase1.Instance(),
+		 "u2": Phase1.Instance(),
+		 "p": Phase1.Instance(),
+		 "stream": Phase1.Instance(),
+		 "mesh": Phase1.Instance(),
+		 "error": Phase1.Instance() }
+
+
+    def act(self, input):
+        return
+
+    def isAccept(self):
+        return False
+
+
     def plotu1():
       pass
+      varu1 = sol.u(1);
 
     def plotu2():
       pass
+      varu2 = sol.u(2);
 
     def plotp():
       pass
+      varp = sol.p();
 
     def plotstream():
       pass
