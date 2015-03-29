22db38e94adeb0bf98125c4eb956d2ff51e7d8d1
derekschlabach <derekschlabach@gmail.com>
2015-03-29 17:39:59 -0500

    stash

--- Program/RunTests.py
+++ Program/RunTests.py
@@ -1,12 +1,12 @@
-#from ParserTests import *
-#from PlotTests import *
+from ParserTests import *
+from PlotTests import *
 #from LoadSaveTests import *
 #from RefineTests import *
 import unittest
 
 
-#testSuite = unittest.makeSuite(TestPlot)
-#testSuite.addTest(unittest.makeSuite(ParserTests))
+testSuite = unittest.makeSuite(TestPlot)
+testSuite.addTest(unittest.makeSuite(ParserTests))
 #testSuite.addTest(unittest.makeSuite(TestLoadSave))
 #testSuite.addTest(unittest.makeSuite(TestRefine))
 
