52b4026d8c25fc347d758a8d3b5a3a05aeeb6ab3
Scott Wurtz <scott.wurtz@my.wheaton.edu>
2015-03-29 14:29:12 -0500

    merging

--- CamelliaSolver.py
+++ CamelliaSolver.py
@@ -1,20 +1,6 @@
 import Driver
 
 
-def main():
-    """Here. We. Go."""
-
-	state = initial();
-	
-	printf("Welcome to the PyCamellia incompressible flow solver!\n");
-	state.prompt();
-	inputString = raw_input("");
-
-	state = state.parse(inputString);
-
-	
-    pass
-    #this statement does nothing
 
 Driver.main()
 
