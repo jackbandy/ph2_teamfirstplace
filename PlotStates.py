f1e7edf1c8bad6df6fac8b6c7729572d4af14647
Jack Bandy <jackbandy@icloud.com>
2015-03-29 08:53:21 -0500

    Re-added all work to PlotStates after losing it. No walls were punched in the making of this commit.

--- PlotStates.py
+++ PlotStates.py
@@ -3,11 +3,12 @@
 
 from Singleton import *
 
+plotType = ''
 
 @Singleton
 class PromptPlot:
     def prompt(self):
-        return "Yo options fo plottin now iz: u1, u2, p, stream, mesh, error"
+        return "Options for plotting now are: u1, u2, p, stream, mesh, error"
 
     def getDict(self):
         return  { "u1": Plotted.Instance(),
@@ -19,54 +20,110 @@ class PromptPlot:
                 }
 
     def act(self, input):
-        #set the plotType so Plotted.py knows what to plot
-        Data.plotType = input
-        # if(input = "u1")
-        return
+        if input is "u1":
+	  plotType = 'u1'
+	  plt_soln = Function.solution(form.u(1),form.solution()) 
+	  plotstd()
+        if input is "u2":
+	  plotType = 'u2'
+	  plt_soln = Function.solution(form.u(2),form.solution()) 
+	  plotstd()
+        if input is "p": 
+	  plotType = 'p'
+	  plt_soln = Function.solution(form.p(),form.solution()) 
+	  plotstd()
+        if input is "stream":
+	  plotType = 'Sream'
+	  stream = form.streamSolution()
+	  stream.solve()
+	  plt_soln = Function.solution(form.streamPhi(),stream) 
+	  plotstd()
+        if input is "mesh":
+	  plotType = 'Mesh'
+	  pass
+        if input is "error":
+	  plotType = 'Error'
+	  pass
 
 
     def isAccept(self):
         return False
 
 
-    def plotu1():
-      spaceDim = 2
-      useConformingTraces = True
-      mu = 1.0
-      form = StokesVGPFormulation(spaceDim,useConformingTraces,mu)
-      
-      sol = form.solution();
-      loaded = Solution.load(form.bf(), "sample")
+    def plotstd():
+	xpoints = []
+	ypoints = []
+	zvalues = []
+	activeCellIDs = mesh.getActiveCellIDs()
+	badv = [[-1.,-1.],[1.,-1.],[1.,1.],[-1.,1.]]
+	for cellID in activeCellIDs:
+	  goodv = array(mesh.verticesForCell(cellID))
+	  (values,points) = u1_soln.getCellValues(mesh,cellID,badv)
+	  totColor = 0;
+	  for val in values:
+	    totColor += val
+	  for point in points:
+	    xpoints.append(point[0])
+	    ypoints.append(point[1])
+	  zvalues.append(totColor / len(values))
+	
+	
+	colInd = 0
+	xpoints = sorted(list(set(xpoints)))
+	ypoints = sorted(list(set(ypoints)))
+	cvmin = 0;
+	cvmax = 0;
+	plotColors = []
+	for i in range(0, (len(ypoints)-1)):
+	  plotColors.append([])
+	  for j in range(0, (len(xpoints)-1)):
+	    plotColors[i].append(zvalues[colInd])
+	    if(zvalues[colInd] < cvmin):
+	      cvmin = zvalues[colInd]
+	    if(zvalues[colInd] > cvmax):
+	      cvmax = zvalues[colInd]
+	    colInd += 1
+	
+	
+	c = plt.pcolormesh(array(xpoints), array(ypoints), array(plotColors), edgecolors='k', linewidths=2, cmap='afmhot', vmin=cvmin, vmax=cvmax)
+	
+	plt.title(plotType)
+	plt.xticks(xpoints) #x ticks are xpoints
+	plt.yticks(ypoints)
+	plt.xlim(0, xpoints[len(xpoints)-1]) #sorted, so max = last item
+	plt.ylim(0, xpoints[len(xpoints)-1])
+	plt.show() #show the plot
+	
 
-      varu1 = sol.u(1);
-      u1_soln = Function.solution(form.u(1),form.solution()) 
-       
-      refCellVertexPoints = [[-1.,-1.],[1.,-1.],[1.,1.],[-1.,1.]]; 
-      mymesh = u1_soln.mesh()
-      activeCellIDs = mymesh.getActiveCellIDs() 
-      for cellID in activeCellIDs: 
-        (values,points) = u1_soln.getCellValues(mesh,cellID,refCellVertexPoints) 
-        print("CellID %i:" % cellID) 
-        print(values) 
-        print(points) 
 
-
-    def plotu2():
-      pass
-      varu2 = sol.u(2);
-
-
-    def plotp():
-      pass
-      varp = sol.p();
-
-
-    def plotstream():
-      pass
 
 
     def plotmesh():
-      pass
+	xpoints = []
+        ypoints = []
+        zvalues = []
+        activeCellIDs = mesh.getActiveCellIDs()
+        for cellID in activeCellIDs:
+          goodv = mesh.verticesForCell(cellID)
+          for val in values:
+            totColor += val
+          for point in goodv:
+            xpoints.append(point[0])
+            ypoints.append(point[1])
+        zvalues = zeros((len(xpoints)-1, len(ypoints)-1))
+
+        colInd = 0
+        xpoints = sorted(list(set(xpoints)))
+        ypoints = sorted(list(set(ypoints)))
+        c = plt.pcolormesh(array(xpoints), array(ypoints), array(zvalues), edgecolors='k', linewidths=2, 
+                           cmap='bwr', vmin='-100', vmax='100') 
+
+        plt.title('Mesh')
+        plt.xticks(xpoints)
+        plt.yticks(ypoints)
+        plt.xlim(0, xpoints[len(xpoints)-1])
+        plt.ylim(0, ypoints[len(ypoints)-1])
+        plt.show()
 
 
     def ploterror():
@@ -80,8 +137,7 @@ class Plotted:
         return 
 
     def getDict(self):
-        return { "": Phase1.Instance() }
-
+        return { "": PhaseStates.Phase2.Instance() }
 
     def act(self, input):
         return
