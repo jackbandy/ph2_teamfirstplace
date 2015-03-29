dd6747f3fd82d5d76ac5ea6e842222567482aea9
Jack Bandy <jackbandy@icloud.com>
2015-03-29 14:54:29 -0500

    Added PlotTests.py and TestingPlan.py, removed my preiliminary files, updated PlanOutline.txt

---
+++ PlotTests.py
@@ -0,0 +1,11 @@
+import PlotStates
+
+#ideally, load stuff into data before the next part
+
+st = PlotStates.PromptPlot.Instance()
+st.act("u1")
+st.act("u2")
+st.act("p")
+st.act("stream")
+st.act("mesh")
+st.act("error")
