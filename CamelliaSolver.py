60007b3a78085009c83698dd82e0388b83e93348
Scott Wurtz <scott.wurtz@my.wheaton.edu>
2015-03-29 14:25:38 -0500

    CamelliaSolver

--- CamelliaSolver.py
+++ CamelliaSolver.py
@@ -1,7 +1,20 @@
 from PyCamellia import *
 import PyCamellia
 
+
+
+
 def main():
     """Here. We. Go."""
+
+	state = initial();
+	
+	printf("Welcome to the PyCamellia incompressible flow solver!\n");
+	state.prompt();
+	inputString = raw_input("");
+
+	state = state.parse(inputString);
+
+	
     pass
     #this statement does nothing
